fc70b830f3529f2e948eb03f6c3c46a064adb183
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-19 15:00:31 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_055747) do
+ActiveRecord::Schema.define(version: 2019_12_19_055957) do
 
   create_table "cart_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "shopping_cart_item_fields"
   end
 
   create_table "carts", force: :cascade do |t|
